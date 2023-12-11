e1aa187b6d6f76b3f14653b7c9afc2440f979fcd
Csyrakepu <bence.masa@gmail.com>
2023-12-11 18:45:36 +0100

    Day 9

--- AdventOfCode2023/Day9.cs
+++ AdventOfCode2023/Day9.cs
@@ -3,17 +3,83 @@ class Day9
 {
     public static int Part1()
     {
-        string[] lines = Source.GetSourceLines(9);
-        
+        string[] lines = Source.GetSourceLines(9);        
+
+        var sequences = new List<Sequence>();
         foreach (string line in lines)
         {
             int[] numbers = line.Split(' ').Select(int.Parse).ToArray();
-            
+            sequences.Add(new Sequence(numbers));
         }
+        return sequences.Sum(sequence => sequence.GetNext());
     }
 
     public static int Part2()
     {
-        return 0;
+        string[] lines = Source.GetSourceLines(9);
+
+        var sequences = new List<Sequence>();
+        foreach (string line in lines)
+        {
+            int[] numbers = line.Split(' ').Select(int.Parse).ToArray();
+            sequences.Add(new Sequence(numbers));
+        }
+        return sequences.Sum(sequence => sequence.GetPrevious());
+    }
+}
+
+class Sequence
+{
+    public List<int[]> lists;
+    int initialLength;
+
+    public Sequence(int[] initialSequence)
+    {
+        lists = new List<int[]> {initialSequence };
+        initialLength = initialSequence.Length;
+        FillSequences();
+    }
+
+    void FillSequences()
+    {
+        bool done = false;
+        while (!done)
+        {
+            int[] currentArray = new int[lists[^1].Length - 1];
+            for (int i = 1; i < lists[^1].Length; i++)
+            {
+                currentArray[i - 1] = lists[^1][i] - lists[^1][i - 1];
+            }
+            lists.Add(currentArray);
+            done = lists[^1].All(n => n == 0);
+        }               
+    }
+
+    public int GetNext()
+    {
+        for (int i = lists.Count-1; i > 0; i--)
+        {
+            int item = lists[i][^1] + lists[i - 1][^1];
+            lists[i-1] = lists[i].Append(item).ToArray();
+        }
+        return lists[0][^1];        
+    }
+
+    public int GetPrevious()
+    {
+        for (int i = lists.Count - 1; i > 0; i--)
+        {
+            int item = lists[i - 1][0] - lists[i][0];
+
+            List<int> copy = lists[i-1].ToList();
+            copy.Insert(0, item);
+            lists[i - 1] = copy.ToArray();
+        }
+        return lists[0][0];
+    }
+
+    public override string ToString()
+    {
+        return string.Join(" ", lists[0]);
     }
 }
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var output = Day9.Part1();
+        var output = Day9.Part2();
         Console.WriteLine(output);
     }
 }
